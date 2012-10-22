218d6342e3a638b2ca5cd611a7a80d7f08f9438e
Kai Lieth <git@radiant-cove-7007>
2012-10-22 12:48:14 -0400

    syntax again

--- FillUser.js
+++ FillUser.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
 	var curUser = User.all['jbrown'];
-	$("#user").setText("Hello, " + curUser.name);
-	$("#userName").setText("User Name: " + curUser.userName);
-	$("#user").setText("About me: " + curUser.about);
-	$("#user").setText("Account created: " + curUser.created);
-	$("#user").setText("Age: " + curUser.age);
-	$("#location").setText("Location: " + curUser.location);
+	$("#user").text("Hello, " + curUser.name);
+	$("#userName").text("User Name: " + curUser.userName);
+	$("#user").text("About me: " + curUser.about);
+	$("#user").text("Account created: " + curUser.created);
+	$("#user").text("Age: " + curUser.age);
+	$("#location").text("Location: " + curUser.location);
 
 });
