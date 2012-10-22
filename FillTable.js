89afa356b72641f2d75f10a62b036b4b69b913e9
Kai Lieth <git@radiant-cove-7007>
2012-10-21 22:03:33 -0400

    Added slashes to close tags.

--- FillTable.js
+++ FillTable.js
@@ -3,12 +3,12 @@
  */
 $(document).ready(function() {
 	jQuery.each(Song.all, function(index, song)){
-		$("#SongInfo").append("<tr><td>" + song.title + "<td>");
-		$("#SongInfo").append("<td>" + song.composer + "<td>");
-		$("#SongInfo").append("<td>" + song.tempo + " BPM" + "<td>");
-		$("#SongInfo").append("<td>" + song.genre + "<td>");
-		$("#SongInfo").append("<td>" + song.startDate + "<td>");
-		$("#SongInfo").append("<td>" + song.comments + "<td><tr>");
+		$("#SongInfo").append("<tr><td>" + song.title + "</td>");
+		$("#SongInfo").append("<td>" + song.composer + "</td>");
+		$("#SongInfo").append("<td>" + song.tempo + " BPM" + "</td>");
+		$("#SongInfo").append("<td>" + song.genre + "</td>");
+		$("#SongInfo").append("<td>" + song.startDate + "</td>");
+		$("#SongInfo").append("<td>" + song.comments + "</td></tr>");
 	}
 
 });
