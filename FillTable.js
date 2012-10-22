648c92a3b99661b100715481d2389f4e5016381f
esabel <esabel@live.unc.edu>
2012-10-22 02:01:03 -0400

    FillTable.js updated

--- FillTable.js
+++ FillTable.js
@@ -2,8 +2,13 @@
  * Populates tables as necessary.
  */
 $(document).ready(function() {
-	jQuery.each(Song.all, function(index, value)){
-		
+	jQuery.each(Song.all, function(index, song)){
+		$("#SongInfo").append("<tr><td>" + song.title + "<td>");
+		$("#SongInfo").append("<td>" + song.composer + "<td>");
+		$("#SongInfo").append("<td>" + song.tempo + " BPM" + "<td>");
+		$("#SongInfo").append("<td>" + song.genre + "<td>");
+		$("#SongInfo").append("<td>" + song.startDate + "<td>");
+		$("#SongInfo").append("<td>" + song.comments + "<td><tr>");
 	}
 
 });
