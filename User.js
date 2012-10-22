6ebcac35fe346030730921579d4e4a9e9094a4cf
Kai Lieth <git@radiant-cove-7007>
2012-10-22 12:47:11 -0400

    Fixed syntax error

--- User.js
+++ User.js
@@ -16,6 +16,7 @@ var User = function(userName, name, about, date, age, location){
 	
 }
 
+User.all = {};
 User.all['jbrown'] = new User('jbrown', 'John Brown', '', '2012-10-22', 23, 'Chapel Hill');
 
 var addCurrentWork = function(song){
