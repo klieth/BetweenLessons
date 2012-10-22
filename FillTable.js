783366bd32a08f3d965c83b816df3fdab6f1ebfc
Kai Lieth <git@radiant-cove-7007>
2012-10-21 22:16:28 -0400

    trying to figure out jquery...

--- FillTable.js
+++ FillTable.js
@@ -3,12 +3,21 @@
  */
 $(document).ready(function() {
 	jQuery.each(Song.all, function(index, song)){
-		$("#SongInfo").append("<tr><td>" + song.title + "</td>");
-		$("#SongInfo").append("<td>" + song.composer + "</td>");
-		$("#SongInfo").append("<td>" + song.tempo + " BPM" + "</td>");
-		$("#SongInfo").append("<td>" + song.genre + "</td>");
-		$("#SongInfo").append("<td>" + song.startDate + "</td>");
-		$("#SongInfo").append("<td>" + song.comments + "</td></tr>");
+		var newrow = $("<tr></tr>");
+		$("#SongInfo").append(newrow);
+		newrow.append("<td>" + song.title + "</td>");
+		newrow.append("<td>" + song.composer + "</td>");
+		newrow.append("<td>" + song.tempo + " BPM</td>");
+		newrow.append("<td>" + song.genre + "</td>");
+		newrow.append("<td>" + song.startDate + "</td>");
+		newrow.append("<td>" + song.comments + "</td>");
+		
+		//$("#SongInfo").append("<tr><td>" + song.title + "</td>");
+		//$("#SongInfo").append("<td>" + song.composer + "</td>");
+		//$("#SongInfo").append("<td>" + song.tempo + " BPM" + "</td>");
+		//$("#SongInfo").append("<td>" + song.genre + "</td>");
+		//$("#SongInfo").append("<td>" + song.startDate + "</td>");
+		//$("#SongInfo").append("<td>" + song.comments + "</td></tr>");
 	}
 
 });
