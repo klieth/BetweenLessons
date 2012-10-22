1a3b4b05ec0822291026eaf4fc1150044bf37f3d
Kai Lieth <git@radiant-cove-7007>
2012-10-21 22:23:42 -0400

    again

--- FillTable.js
+++ FillTable.js
@@ -4,13 +4,13 @@
 $(document).ready(function() {
 	jQuery.each(Song.all, function(index, song)){
 		var newrow = $("<tr></tr>");
-		$("#SongInfo").append(newrow);
 		newrow.append("<td>" + song.title + "</td>");
 		newrow.append("<td>" + song.composer + "</td>");
 		newrow.append("<td>" + song.tempo + " BPM</td>");
 		newrow.append("<td>" + song.genre + "</td>");
 		newrow.append("<td>" + song.startDate + "</td>");
 		newrow.append("<td>" + song.comments + "</td>");
+		$("#SongInfo").append(newrow);
 		
 		//$("#SongInfo").append("<tr><td>" + song.title + "</td>");
 		//$("#SongInfo").append("<td>" + song.composer + "</td>");
