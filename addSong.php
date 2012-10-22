f4718c2f9a74bc1c5a2360ac1352d12f9b5aab2c
Elijah Abel <esabel@live.unc.edu>
2012-10-22 15:36:56 -0400

    line breaks added to addSong.php

--- addSong.php
+++ addSong.php
@@ -1,12 +1,12 @@
 <?php include 'header.php' ?>
 	<div id="content">
 		<h2 id="contentTitle">Add Song</h2>
-			<label>Song Title: <input type="text" name="title"></label>
-			<label>Composer: <input type="text" name="composer"></label>
-			<label>Song Tempo (bpm): <input type="text" name="tempo"></label>
-			<label>Genre: <input type="text" name="genre"></label>
-			<label>Start date (yyyy-mm-dd): <input type="text" name="date"></label>
-			<label>Comments: <input type="text" name="comments"></label>
+			<label>Song Title: <input type="text" name="title"></label><br>
+			<label>Composer: <input type="text" name="composer"></label><br>
+			<label>Song Tempo (bpm): <input type="text" name="tempo"></label><br>
+			<label>Genre: <input type="text" name="genre"></label><br>
+			<label>Start date (yyyy-mm-dd): <input type="text" name="date"></label><br>
+			<label>Comments: <input type="text" name="comments"></label><br>
 			<button type="submit">Add Song</button>
 		</div>
 
