f562e25a70b22626739d29d4046157e8105af3ec
Kai Lieth <klieth@live.unc.edu>
2012-12-06 00:40:47 -0500

    Yet more troubleshooting

--- loadCurrent.js
+++ loadCurrent.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$.ajax({
-		url: 'SongRest.php?uid=1',
-		success: function(data) {
-			$('#test').html(data);
+		url: 'SongRest.php?uid=1'
+	}).done(function(data){
+		if (console && console.log) {
+			console.log("Sample of data:", data.slice (0,3));
 		}
 	});
 });
