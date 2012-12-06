c9f63d229fc9bbd25a5bacaabfdfa83cadd4fc27
esabel <esabel@live.unc.edu>
2012-12-06 01:58:25 -0500

    user registration

--- userLogin.php
+++ userLogin.php
@@ -3,6 +3,7 @@
 	<h2 id="contentTitle">User login</h2>
 			<label>Username: <input id="username" type="text" name="name"></label><br>
 			<label>Password: <input id="userpass" type="text" name="password"></label><br>
-			<input type="submit" value="Login">
+			<input type="submit" value="Login"><br>
+			<label>Not registered yet? <a href="userRegistration.php"> Register here.</a></label>
 			</div>
 <?php include "footer.php" ?>
---
+++ userRegistration.php
@@ -0,0 +1,9 @@
+<?php include 'songHeader.php' ?>
+    <div id="content">
+    	<h2 id="contentTitle">User login</h2>
+			<label>Desired Username: <input id="username" type="text" name="name"></label><br>
+			<label>Desired Password: <input id="userpass" type="text" name="password"></label><br>
+			<label>Retype your password: <input id="reuserpass" type="text" name="repassword"</label>
+			<input type="submit" value="Login">
+    </div>
+<?php include "footer.php" ?>
