174541ad8de2fc6565ee089aade9b83bf5acb396
Kai Lieth <klieth@live.unc.edu>
2012-12-06 00:57:01 -0500

    Try it with the sid...

--- loadCurrent.js
+++ loadCurrent.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	}).done(function(data){
 		$.each(data, function(index, value){
 			console.log(index + ": " + value);
-			$('#test').append($('<p>' + value + '</p>'));
+			$('#test').append($('<p>' + value.sid + '</p>'));
 		});
 	});
 });
