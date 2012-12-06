0af7f1359c669734a924241d0bf39b37630281bc
Kai Lieth <klieth@live.unc.edu>
2012-12-06 00:34:11 -0500

    More test code

--- loadCurrent.js
+++ loadCurrent.js
@@ -1,25 +1,8 @@
 $(document).ready(function() {
-	var ajax_url = 'SongRest.php';
-
-	var data_map = {
-		uid: '1'
-	};
-
-	var settings = {
-		type: 'GET',
-		data: data_map,
-		success: ajax_success_handler,
-		error: ajax_error_handler
-	};
-
-	$.ajax(ajax_url, settings);
-
-	var ajax_success_handler = function(data, textStatus, jqXHR) {
-		console.log("success")
-		$('#test').html(jqXHR.responseText);
-	}
-
-	var ajax_error_handler = function(jqXHR, textStatus, errorThrown) {
-		$('#error').html(jqXHR.responseText);
-	}
+	$.ajax({
+		url: 'SongRest.php?uid=1',
+		success: function(data) {
+			$('#test').html(data);
+		}
+	});
 });
