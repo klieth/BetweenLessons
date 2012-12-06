4d4b60d61818c206f706d907448fa7cd8a82d68c
Kai Lieth <klieth@live.unc.edu>
2012-12-06 00:56:26 -0500

    Now to try it with the value because index works.

--- loadCurrent.js
+++ loadCurrent.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	}).done(function(data){
 		$.each(data, function(index, value){
 			console.log(index + ": " + value);
-			$('#test').append($('<p>' + index + '</p>'));
+			$('#test').append($('<p>' + value + '</p>'));
 		});
 	});
 });
