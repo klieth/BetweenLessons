009cc25fa4fd18e59c96eba937b87ab849e96eb9
Kai Lieth <klieth@live.unc.edu>
2012-12-06 00:48:43 -0500

    Hopefully this each call works!

--- loadCurrent.js
+++ loadCurrent.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$.ajax({
 		url: 'SongRest.php?uid=1'
 	}).done(function(data){
-		if (console && console.log) {
-			console.log("Sample of data:", data.slice (0,3));
-		}
+		$.each(data, function(index, value){
+			console.log(index + ": " + value);
+		});
 	});
 });
